ff66f2674ce996e23142e247d613c56ee5a4a42f
OctavioSesana <osesana@frro.utn.edu.ar>
2026-06-29 10:00:29 -0300

    GREEN: UT - revela todas las ocurrencias de la letra acertada

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,14 +1,22 @@
 export class Ahorcado {
   private palabra: string;
   private vidasRestantes : number = 6;
+  private letrasAdivinadas: string[] = [];
 
   constructor(palabra: string) {
     this.palabra = palabra;
   }
 
-  palabraEnmascarada(): string {
-    return this.palabra.split("").map(() => "_").join(" ");
-  }
+  adivinar(letra: string): void {
+  this.letrasAdivinadas.push(letra.toUpperCase());
+}
+
+palabraEnmascarada(): string {
+  return this.palabra
+    .split("")
+    .map(l => this.letrasAdivinadas.includes(l) ? l : "_")
+    .join(" ");
+}
 
   vidas(): number {
     return this.vidasRestantes;
