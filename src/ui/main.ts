2bbf6b2e91b4453a5df17210105e34a3ff19f5cc
OctavioSesana <osesana@frro.utn.edu.ar>
2026-06-29 10:06:49 -0300

    GREEN: AT2 - El jugador acierta una letra

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -3,8 +3,21 @@ import { Ahorcado } from "../domain/Ahorcado";
 export function render(app: HTMLElement, palabra: string): void {
   const juego = new Ahorcado(palabra);
 
-  app.innerHTML = `
-    <div data-testid="word">${juego.palabraEnmascarada()}</div>
-    <div data-testid="lives">${juego.vidas()}</div>
-  `;
+  function renderizar(): void {
+    app.innerHTML = `
+      <input type="text" />
+      <div data-testid="word">${juego.palabraEnmascarada()}</div>
+      <div data-testid="lives">${juego.vidas()}</div>
+    `;
+
+    const input = app.querySelector("input")!;
+    input.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        juego.adivinar(input.value);
+        renderizar();
+      }
+    });
+  }
+
+  renderizar();
 }
